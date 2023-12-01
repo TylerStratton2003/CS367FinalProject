f73f2d0416b38fc5f20802688cce9a4985af12d3
lqgreen <151587258+lqgreen@users.noreply.github.com>
2023-11-30 21:43:10 -0800

    update

--- React/vapor/src/App.js
+++ React/vapor/src/App.js
@@ -4,9 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Vapor: Game Recommendation System
-        </p>
+        <p>Vapor: Game Recommendation System</p>
         <p>
           <label for="search">Search:</label>
           <input type="text" name="search" id="search"></input>
@@ -27,27 +25,9 @@ function searchResults() {
       <p>Results:</p>
     </header>
     </div>
-  )
-}
-function mostPop() {
-  return (
-    <div className="mostpop">
-    <header className="most-pop">
-      <p>Most Popular:</p>
-    </header>
-    </div>
-  )
+  );
 }
 
-function newest() {
-  return (
-    <div className="newest">
-    <header className="newest">
-      <p>Newest:</p>
-    </header>
-    </div>
-  )
-}
 
 
 export default App;
