9e932d7cc2cd67f37f99c86d641f2d51ee9dc2b8
TylerStratton2003 <tyler.stratton@plu.edu>
2023-12-01 12:29:45 -0800

    Added tags 1 and 2, as well as os checks

--- React/vapor/src/App.js
+++ React/vapor/src/App.js
@@ -8,8 +8,16 @@ function App() {
         <p>
           <label for="search">Search:</label>
           <input type="text" name="search" id="search"></input>
-          <label for="tags">Tags:</label>
-          <input type="checkbox" name="tags" id="tags"></input>
+          <label for="tag1">Tag 1:</label>
+          <input type="text" name="tag1" id="tag1"></input>
+          <label for="tag2">Tag 2:</label>
+          <input type="text" name="tag2" id="tag2"></input>
+          <label for="win">Windows:</label>
+          <input type="checkbox" name="win" id="win" checked></input>
+          <label for="lix">Linux:</label>
+          <input type="checkbox" name="lix" id="lix" checked></input>
+          <label for="mac">Mac:</label>
+          <input type="checkbox" name="mac" id="mac" checked></input>
         </p>
       </header>
       <body>
